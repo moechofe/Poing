bd219f0575d5b6b2d385e69a6fdfb5510ed6a1cb
moechofe <service@moechofe.com>
2013-09-11 20:15:52 +0200

    server: serves static files.

--- server/source/app.d
+++ server/source/app.d
@@ -1,18 +1,24 @@
 import vibe.d;
 
+import vibe.http.fileserver;
+
 void index(HTTPServerRequest req, HTTPServerResponse res)
 {
-    res.writeBody(cast(ubyte[])"Hello, World!", "text/plain");
+	res.writeBody(cast(ubyte[])("Hello, World!"), "text/plain");
 }
 
 shared static this()
 {
-    auto router = new URLRouter;
-    router.get("/", &index);
+	auto router = new URLRouter;
+	router.get("/app/*", serveStaticFiles("../static/"));
+	router.get("/lib/*", serveStaticFiles("../static/"));
+	router.get("/gfx/*", serveStaticFiles("../static/"));
+	router.get("/res/*", serveStaticFiles("../static/"));
+	router.get("/", &index);
 
-    auto settings = new HTTPServerSettings;
-    settings.bindAddresses = ["::1", "127.0.0.1", "188.165.196.107"];
-    settings.port = 8080;
+	auto settings = new HTTPServerSettings;
+	settings.bindAddresses = ["::1", "127.0.0.1", "ici", "static.ici"];
+	settings.port = 8080;
 
-    listenHTTP(settings, router);
+	listenHTTP(settings, router);
 }
